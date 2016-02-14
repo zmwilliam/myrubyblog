f96c7bf77d5e0df02148d3ce56e06149e2be6f9b
zmwilliam <willcxa@gmail.com>
2016-02-14 11:07:00 -0200

    Adding post and category models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @content_first='This is a sample text'
+    @content_second='More sample text'    	
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
