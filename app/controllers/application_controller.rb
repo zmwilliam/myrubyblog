7c1aa8a4e7aed23965d560b9400f891d158c6fae
zmwilliam <willcxa@gmail.com>
2016-02-25 00:30:05 -0300

    Header and sidebar partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :all_categories
+  helper_method :all_posts
+
+  def all_categories
+    @categories = Category.all
+  end
+
+  def all_posts
+    @posts = Post.all
+  end
 end
